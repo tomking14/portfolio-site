a1dae002f5e44d919306e678132b161577065444
tomking14 <tomykastor@gmail.com>
2024-01-10 13:42:24 -0500

    starting

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './banner.css';
+
+
+export default function Banner() {
+  return (
+    <div className='d-flex flex-row banner'>
+        <p>hello</p>
+        <p>testing</p>
+
+      
+    </div>
+  )
+}
