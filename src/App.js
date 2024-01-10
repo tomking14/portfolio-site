0227bfb813892afcc0a16a5eb50b95df704e298b
tomking14 <tomykastor@gmail.com>
2024-01-10 14:21:08 -0500

    more stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Banner from './components/banner/Banner.js';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function App() {
   return (
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './banner.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 
 export default function Banner() {
   return (
-    <div className='d-flex flex-row banner'>
-        <p>hello</p>
-        <p>testing</p>
-
-      
+    <div className='d-flex flex-column justify-content-center banner gap-3'>
+        <h1 className=' title fw-bold custom-left-margin'>Tom Kastoryano</h1>
+        <div className="row">
+        <div className="col-6">
+            <h3 class="custom-left-margin">
+                I'm a recent college graduate that loves frontend development and is looking to expand his horizons
+            </h3>
+        </div>
+    </div>
     </div>
   )
 }
