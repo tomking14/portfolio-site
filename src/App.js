87665330667e4e70307e66f4ce45599867023b31
tomking14 <tomykastor@gmail.com>
2024-01-17 17:20:04 -0500

    working

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
   return (
     <div className='container'>
       <Banner/>
-      <h2>Projects</h2>
+      <h2 className='mb-5'>Projects</h2>
       <Card title={"Internship"} image={blackhole} description={"Helped build a design system consisting of accessible, adaptable, user-friendly UI components and full pages for Cricket developers to use throughout the Cricket website using HTML, CSS, Bootstrap, and JavaScript"}/>
-      <Card title={"Film Site"} image={bottomimage} description={"Helped build a design system consisting of accessible, adaptable, user-friendly UI components and full pages for Cricket developers to use throughout the Cricket website using HTML, CSS, Bootstrap, and JavaScript"}/>
-      <Card title={"Ride Share Application"} image={header} description={"Helped build a design system consisting of accessible, adaptable, user-friendly UI components and full pages for Cricket developers to use throughout the Cricket website using HTML, CSS, Bootstrap, and JavaScript"}/>
+      <Card title={"Film Site"} image={bottomimage} description={"Worked within a Scrum to build a full stack application mimicking a cinema site to order movie tickets using React and Flask with a focus on the frontend side "}/>
+      <Card title={"Ride Share Application"} image={header} description={"Worked with a partner using agile methodology to create an android application letting users create and accept ride requests and offers using Android Studio, and Google Firebase"}/>
 
     </div>
   );
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './banner.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -17,18 +18,26 @@ export default function Banner() {
     <div className='d-flex flex-column justify-content-center banner gap-4'>
         <h1 className='title fw-bold mb-0'>Tom Kastoryano</h1>
         <div className="row">
-        <div className="col-9">
+        <div className="col-10 col-sm-9">
             <h3 class="mb-4">
                 I'm a frontend developer that want to make the internet more pretty and accessible
                 
             </h3>
         </div>
         <div className='d-flex gap-4'>
-            <FontAwesomeIcon icon={faLinkedin} size="3x"/>
-            <FontAwesomeIcon icon={faGithub} size="3x"/>
-            <FontAwesomeIcon icon={faEnvelope} size="3x"/>
-            <FontAwesomeIcon icon={faSquareInstagram} size="3x"/>
+        <a href="https://www.linkedin.com/in/tom-kastoryano-094699234/" target="_blank" rel="noopener noreferrer" className='custom-link'>
+            <FontAwesomeIcon icon={faLinkedin} size="3x" />
+        </a>
+        <a href="https://github.com/tomking14" target="_blank" rel="noopener noreferrer" className='custom-link'>
+          <FontAwesomeIcon icon={faGithub} size="3x"/>
+        </a>
             
+        <a href="mailto:tomykastor@gmail.com" className="custom-link">
+          <FontAwesomeIcon icon={faEnvelope} size="3x" />
+        </a>
+        <a href="https://www.instagram.com/tomykastor/" target="_blank" rel="noopener noreferrer" className='custom-link'>
+            <FontAwesomeIcon icon={faSquareInstagram} size="3x"/>
+        </a>
         </div>
       </div>
     </div>
