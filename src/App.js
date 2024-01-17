387d0e74e5ab4b6cbf38e68bffb4b06a2659032f
tomking14 <tomykastor@gmail.com>
2024-01-17 16:45:12 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Card from './components/cards/Card.js';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import blackhole from './images/blackhole.jpeg'
+import bottomimage from './images/bottom.png'
+import header from './images/headerimage.png'
 
 
 
@@ -10,9 +13,11 @@ function App() {
   return (
     <div className='container'>
       <Banner/>
-      <h2 className='mb-5'>Projects</h2>
-      <Card />
-     
+      <h2>Projects</h2>
+      <Card title={"Internship"} image={blackhole} description={"Helped build a design system consisting of accessible, adaptable, user-friendly UI components and full pages for Cricket developers to use throughout the Cricket website using HTML, CSS, Bootstrap, and JavaScript"}/>
+      <Card title={"Film Site"} image={bottomimage} description={"Helped build a design system consisting of accessible, adaptable, user-friendly UI components and full pages for Cricket developers to use throughout the Cricket website using HTML, CSS, Bootstrap, and JavaScript"}/>
+      <Card title={"Ride Share Application"} image={header} description={"Helped build a design system consisting of accessible, adaptable, user-friendly UI components and full pages for Cricket developers to use throughout the Cricket website using HTML, CSS, Bootstrap, and JavaScript"}/>
+
     </div>
   );
 }
--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import './cards.css';
-import blackhole from '../../images/blackhole.jpeg'
+// import blackhole from '../../images/blackhole.jpeg'
 
-export default function card() {
+export default function Card(props) {
   return (
-    <div className='d-flex flex-column flex-md-row card-container'>
+    <div>
+    <div className='d-flex flex-column flex-md-row card-container mb-5'>
         <div>
-            <h2 className="mb-4">Internship</h2>
-            <p>Helped build a design system consisting of accessible, adaptable, user-friendly UI components and full pages for Cricket developers to use throughout the Cricket website using HTML, CSS, Bootstrap, and JavaScript</p>
+            <h2 className="mb-4">{props.title}</h2>
+            <p>{props.description}</p>
         </div>
-        <img src={blackhole} alt="Black hole" className='image-size'/>
+        <img src={props.image} alt="Black hole" className='image-size'/>
     </div>
+    
+    </div>
+    
   )
 }
