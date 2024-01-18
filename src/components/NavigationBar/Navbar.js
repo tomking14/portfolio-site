31011f0da096fc038eef4aa42cb231a0ca667173
tomking14 <tomykastor@gmail.com>
2024-01-18 15:14:59 -0500

    yay

--- src/components/NavigationBar/Navbar.js
+++ src/components/NavigationBar/Navbar.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './navbar.css'
 export default function Navbar({ scrollToRef, refs }) {
+
   return (
-    <div className='d-flex justify-content-between navbar-styling'>
+    <div className={`d-flex justify-content-between navbar-styling`}>
         <a href="#projects" className='navbar-link'>
              <h3 className='mt-3 regular-text'>TK</h3>
         </a>
@@ -13,7 +14,7 @@ export default function Navbar({ scrollToRef, refs }) {
         <a href="#projects" className='navbar-link'>
             <h4 className='regular-text'>About Me</h4>
         </a>
-        <a href="https://www.linkedin.com/in/tom-kastoryano-094699234/" target="_blank" rel="noopener noreferrer" className='navbar-link'>
+        <a href="https://www.linkedin.com/in/tom-kastoryano-094699234/" target="_blank" rel="noopener noreferrer" className='navbar-link-resume'>
             <h4 className='regular-text resume-button'>Resume</h4>
         </a>
       
