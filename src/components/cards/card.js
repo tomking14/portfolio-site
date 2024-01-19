cd8d46ab2039fe07d2d543def797fd045e263101
tomking14 <tomykastor@gmail.com>
2024-01-19 14:18:40 -0500

    not sure whats goin gon

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -6,7 +6,7 @@ export default function Card(props) {
   return (
     <div {...props} className='d-flex flex-column flex-md-row card-container mb-5'>
         <div>
-            <h2 className="mt-4 mb-3 medium-text">{props.title}</h2>
+            <h2 className="mt-4 mb-2 medium-text">{props.title}</h2>
             <p className='light-text'>{props.description}</p>
         </div>
         <img src={props.image} alt= {props.imageAlt} className='image-size'/>
