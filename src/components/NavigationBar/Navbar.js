927104395e18d8c41de70751b01f377c787c2845
tomking14 <tomykastor@gmail.com>
2024-01-19 15:32:23 -0500

    more accessible

--- src/components/NavigationBar/Navbar.js
+++ src/components/NavigationBar/Navbar.js
@@ -4,16 +4,16 @@ export default function Navbar({ scrollToRef, refs }) {
 
   return (
     <div className={`d-flex justify-content-between navbar-styling`}>
-        <a href="#banner" onClick={() => scrollToRef(refs.bannerRef)} className='navbar-link'>
+        <button onClick={() => scrollToRef(refs.bannerRef)} className='navbar-link'>
              <h3 className='mt-3 regular-text'>TK</h3>
-        </a>
+        </button>
         <div className=' mt-3 d-flex justify-content-end gap-3 gap-lg-5'>
-        <a href="#projects" onClick={() => scrollToRef(refs.projectsRef)} className='navbar-link'>
+        <button onClick={() => scrollToRef(refs.projectsRef)} className='navbar-link'>
             <h4 className='regular-text'>Projects</h4>
-        </a>
-        <a href="#projects" className='navbar-link'>
+        </button>
+        <button className='navbar-link'>
             <h4 className='regular-text'>About Me</h4>
-        </a>
+        </button>
         <a href="https://www.linkedin.com/in/tom-kastoryano-094699234/" target="_blank" rel="noopener noreferrer" className='navbar-link-resume'>
             <h4 className='regular-text resume-button'>Resume</h4>
         </a>
