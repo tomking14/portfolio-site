d75644afecbf70c68bcb196ef7f53c8e8eec878a
tomking14 <tomykastor@gmail.com>
2024-01-23 13:41:40 -0500

    finishing up?

--- src/components/NavigationBar/Navbar.js
+++ src/components/NavigationBar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './navbar.css'
 export default function Navbar({ scrollToRef, refs }) {
 
@@ -15,7 +15,7 @@ export default function Navbar({ scrollToRef, refs }) {
         <button onClick={() => scrollToRef(refs.aboutMeRef)} className='navbar-link'>
             <h4 className='regular-text'>About Me</h4>
         </button>
-        <a href="https://www.linkedin.com/in/tom-kastoryano-094699234/" target="_blank" rel="noopener noreferrer" className='navbar-link-resume'>
+        <a href='/resume.html' target="_blank" rel="noopener noreferrer" className='navbar-link-resume'>
             <h4 className='regular-text resume-button'>Resume</h4>
         </a>
       
